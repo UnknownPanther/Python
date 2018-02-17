da70bc84b3b3de2b793a40cc2a5658b970303e16
UnknownPanther <36569481+UnknownPanther@users.noreply.github.com>
2018-02-17 20:10:24 +0200

    Add files via upload

---
+++ Matrix.py
@@ -0,0 +1,17 @@
+# Program which displays Matrix using nested loop
+# 17.02.2018 | (3)
+
+x = [[12,7],
+    [4 ,5],
+    [3 ,8]]
+
+result = [[0,0,0],
+         [0,0,0]]
+
+for i in range(len(x)):
+    for j in range(len(x[0])):
+          result[j][i] = x[i][j]
+
+for r in result:
+    print(r)
+
